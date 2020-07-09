b4b46e5673811004c09efb8f2d72e380214c84df
cosasparawebs <66963970+cosasparawebs@users.noreply.github.com>
2020-07-09 17:26:19 -0300

    Add files via upload

---
+++ formulario-mail/app.js
@@ -0,0 +1,31 @@
+const nombre = document.getElementById('name');
+const email = document.getElementById('email');
+const pass = document.getElementById('password');
+const form = document.getElementById('form');
+const parrafo = document.getElementById('warnings');
+
+form.addEventListener("submit", e=>{
+    e.preventDefault();
+    let warnings = '';
+    let entrar = false;
+    let regexEmail = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
+    parrafo.innerHTML = "";
+    if(nombre.value.length <6){
+        warnings += `EL nombre no es valido <br>`;
+        entrar = true;
+    }
+    console.log(regexEmail.test(email.value))
+    if(!regexEmail.test(email.value)){
+        warnings += `EL email es valido <br>`;
+        entrar = true;
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`;
+        entrar = true;
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings;
+    } else{
+        parrafo.innerHTML = 'Enviado';
+    }
+})
